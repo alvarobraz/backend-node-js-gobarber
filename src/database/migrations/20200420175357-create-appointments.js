36d57f5930aed0a6be3ca9ca2a2f425fad67e3d2
alvarobraz <alvarobraz83@gmail.com>
2020-07-08 23:32:21 -0300

    Backend desenvolvido em Node.js construindo uma API Rest

---
+++ src/database/migrations/20200420175357-create-appointments.js
@@ -0,0 +1,47 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('appointments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      // usuário que ta fazendo o agendamento
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      // prestador de servico que vai atender o agendamento.
+      provider_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'users', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+      },
+      date: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: (queryInterface) => {
+    return queryInterface.dropTable('appointments');
+  },
+};
